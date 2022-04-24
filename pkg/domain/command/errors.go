efde1b65a00c45e18ebbf0ca0132a942bc1e3b59
ChromaMaster <vladtel.dev@gmail.com>
2022-04-24 21:08:16 +0200

    refactor: remove command package

--- pkg/domain/command/errors.go
+++
@@ -1,9 +0,0 @@
-package command
-
-import "errors"
-
-// TODO(fede): Tiene sentido mover esto aquí?
-var (
-	ErrCommandNotFound          = errors.New("command not found")
-	ErrCommandAlreadyRegistered = errors.New("command already registered")
-)
